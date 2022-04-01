154714d6d723f68beed2ac44d1632d28681ad2f6
holdena12 <102497243+holdena12@users.noreply.github.com>
2022-03-31 20:54:21 -0600

    we added easy mode

--- wordle.py
+++ wordle.py
@@ -1,22 +1,46 @@
 import random
 
+# splits a word into indivual characters
 def splitWord(word):
     return [char for char in word]
 
- # read dictionary and assign to lines
-with open('dict.txt') as f:
-    lines = f.readlines()
-f.close()
+# strips a list of words from any end of line tokens like \n
+def stripWords(wordList):
+    count = 0
+    validWords = set()
+    for line in wordList:
+        count += 1
+        validWords.add(line.strip().lower())
+    return validWords
 
-# print lines in file
-count = 0
-validWords = set()
-for line in lines:
-    count += 1
-    validWords.add(line.strip())
-    #print(f'word {count}: {line}')  
+# reads a file which is expected to be a list of words seperated by new lines
+def readFile(fileName):
+    # read dictionary and assign to lines
+    with open(fileName) as f:
+        lines = f.readlines()
+        strippedWords = stripWords(lines)
+    f.close()
+    return strippedWords
+
+#the set of all valid words that a player may guess
+validWords = readFile('dict.txt')
+
+#load a set of potential answer words
+
+answerWords=readFile('words.txt')
+
+
+    #print(f'word {count}: {line}')
+
+# get an input from user to indicate easy or hard mode
+hardMode = input('do you want to play on hard mode, Enter yes if so: ').lower()
+# if easy mode, use 'answerWords' for ansewr, if hard use validWords  
+answer=''
+if (hardMode=='yes'):
+    anwser=random.choice(list(validWords))
 # select random word from dictinary
-anwser=random.choice(list(validWords))
+else:
+    anwser=random.choice(list(answerWords))
 #print(anwser)
 
 # read input from user to get test word
@@ -25,14 +49,14 @@ for i in range(6):
     if (solved):
         break
     while True:
-        wordChoice = input('Enter Guess: ')
+        wordChoice = input('Enter Guess: ').lower()
         if wordChoice in validWords:
             print('valid word entered')
             charPos = 0
             for c in splitWord(wordChoice):
                 
-                # print 1 if the character is in the answer AND in the correct spot
-                # print 2 if the character is in the answer but not in correct spot
+                # print 2 if the character is in the answer AND in the correct spot
+                # print 1 if the character is in the answer but not in correct spot
                 # print 0 if the character is not in the answer
                 if (c in anwser):
                     # if the char in position charPos in answer = 0 print 2, else print 1
